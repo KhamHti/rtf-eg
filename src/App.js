d814184892e98a5fa0c08131e8f0bfe60685da41
KhamHti <90329989+KhamHti@users.noreply.github.com>
2023-08-08 01:16:17 +0630

    secondary commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import AddPostForm from "./screens/AddPostForm";
+import PostsList from "./screens/PostsList";
+
+function App() {
+  return (
+    <main className="App">
+      <AddPostForm/>
+      <PostsList />
+    </main>
+  );
+}
+
+export default App;
