081dbe0e3ff7a877a76dada73cfe3f994a63cabc
KhamHti <90329989+KhamHti@users.noreply.github.com>
2023-08-08 22:29:03 +0630

    screens

---
+++ src/screens/AddPostForm.js
@@ -0,0 +1,78 @@
+import React from "react";
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { addNewPost } from "../redux/features/posts/postsSlice";
+
+const AddPostForm = () => {
+  const users = useSelector((state) => state.users);
+  const dispatch = useDispatch();
+
+  // console.log("asdf",users);
+
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+  const [userId, setUserId] = useState("");
+  const [addRequest, setAddRequest] = useState("");
+
+  const onTitleChanged = (e) => setTitle(e.target.value);
+  const onContentChanged = (e) => setContent(e.target.value);
+  const onUsersChanged = (e) => setUserId(e.target.value);
+
+  const canSave = [title, content, userId].every(Boolean) && addRequest === "";
+
+  const onSavePostClicked = () => {
+    if (canSave) {
+      try {
+        setAddRequest("Pending");
+        dispatch(addNewPost({ title, body: content, userId })).unwrap();
+
+        setTitle("");
+        setContent("");
+        setUserId("");
+      } catch (error) {
+        console.log("failed to save the post", error);
+      } finally {
+        setAddRequest("");
+      }
+    }
+  };
+
+  const usersOptions = users.map((user) => (
+    <option key={user.id} value={user.id}>
+      {user.name}
+    </option>
+  ));
+
+  return (
+    <section>
+      <h2>Add a New Post</h2>
+      <form>
+        <label htmlFor="postTitle">Post Title:</label>
+        <input
+          type="text"
+          id="postTitle"
+          name="postTitle"
+          value={title}
+          onChange={onTitleChanged}
+        />
+
+        <label htmlFor="postAuthor">User:</label>
+        <select id="postAuthor" value={userId} onChange={onUsersChanged}>
+          <option value=""></option>
+          {usersOptions}
+        </select>
+        <label htmlFor="postContent">Content:</label>
+        <textarea
+          id="postContent"
+          name="postContent"
+          value={content}
+          onChange={onContentChanged}
+        />
+        <button type="button" onClick={onSavePostClicked} disabled={!canSave}>
+          Save Post
+        </button>
+      </form>
+    </section>
+  );
+};
+export default AddPostForm;
---
+++ src/screens/PostsList.js
@@ -0,0 +1,54 @@
+import React, {useEffect} from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { fetchPosts } from "../redux/features/posts/postsSlice";
+import PostsExcerpt from "../components/PostsExcerpt";
+
+const PostsList = () => {
+  const dispatch = useDispatch();
+
+  const posts = useSelector((state) => state.posts.posts);
+  const status = useSelector((state) => state.posts.status);
+  const error = useSelector((state) => state.posts.error);
+
+
+  useEffect(()=> {
+    if(status === 'idle'){
+      dispatch(fetchPosts());
+    }
+  },[status,dispatch]);
+
+  let content;
+  if(status === 'loading'){
+    content = <p>loading...</p>;
+  }else if (status === "success"){
+    const orderedPosts = posts.slice().sort((a,b) => b.date.localeCompare(a.date));
+    content = orderedPosts.map((post) => (
+      <PostsExcerpt key={post.id} post={post}/>
+    ))
+  }else if (status === "failed"){
+    content = <p>{error}</p>
+  }
+
+  // const orderedPosts = posts.slice().sort((a,b) => b.date.localeCompare(a.date));
+  
+  // const renderPosts = orderedPosts.map((post) => (
+  //   <article key={post.id}>
+  //     <h3>{post.title}</h3>
+  //     <p>{post.content.substring(0, 100)}</p>
+  //     <p className="postCredit">
+  //       <PostUsers userId={post.userId} />
+  //       <TimeAgo timestamp={post.date} />
+  //     </p>
+  //       <ReactionsButton post={post} />
+  //   </article>
+  // ));
+
+  return (
+    <section>
+      <h2>Posts</h2>
+      {content}
+    </section>
+  );
+};
+
+export default PostsList;
